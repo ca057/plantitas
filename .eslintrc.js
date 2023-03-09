3453fc561910417d3adb32810510bb6c1662e90c
Christian Ost <hallo@christianost.de>
2023-03-09 23:23:35 +0100

    feat: basic structure and config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'plugin:react/jsx-runtime'],
+  rules: {
+    'prettier/prettier': 0,
+  },
 };
